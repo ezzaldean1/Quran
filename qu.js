025661505bda787a3f8458c880a5864249cea924
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-08 03:03:29 +0300

    Update qu.js

--- qu.js
+++ qu.js
@@ -33,6 +33,87 @@ client.on("guildCreate", guild => {
     });
     
 
+
+
+client.on("message",function(message) {
+    if(message.content.startsWith(prefix + "info")) {
+           let uptime = client.uptime;
+
+    let days = 0;
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let notCompleted = true;
+
+    while (notCompleted) {
+
+        if (uptime >= 8.64e+7) {
+
+            days++;
+            uptime -= 8.64e+7;
+
+        } else if (uptime >= 3.6e+6) {
+
+            hours++;
+            uptime -= 3.6e+6;
+
+        } else if (uptime >= 60000) {
+
+            minutes++;
+            uptime -= 60000;
+
+        } else if (uptime >= 1000) {
+            seconds++;
+            uptime -= 1000;
+
+        }
+
+        if (uptime < 1000)  notCompleted = false;
+
+    }
+let ms = 1000;
+let v1 = new Discord.RichEmbed()
+  v1.setTimestamp(new Date())
+  v1.setColor("RED")
+  v1.setDescription('***__ Collecting Data __***')
+  v1.setFooter("${client.user.username}") 
+let heroo = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setTimestamp(new Date())
+.setThumbnail(client.user.avatarURL)
+.setAuthor(client.user.username,client.user.avatarURL)
+.addField("MyPrefix :",`**[ ${prefix} ]**`,true)
+.addField("Guilds :","**[ "+client.guilds.size+" ]**",true)
+.addField("Channels :","**[ "+client.channels.size+" ]**",true)
+.addField("Users :","**[ "+client.users.size+" ]**",true)
+.addField("MyName : ","**[ "+client.user.username+" ]**",true)
+.addField("MyID :","**[ "+client.user.id+" ]**",true)
+.addField("RamUsage :",`**[ ${(process.memoryUsage().rss / 1048576).toFixed()}MB ]**`,true)
+.addField("UpTime :",`**[** **Days:** \`${days}\` **Hours:** \`${hours}\` **Minutes:** \`${minutes}\` **Seconds:** \`${seconds}\` **]**`,true)
+.setFooter("Quran-Bot-info")
+  message.channel.send({embed:v1}).then(m => m.edit({embed:heroo})),ms; 
+    }
+});
+function timeCon(time) {
+    let days = Math.floor(time % 31536000 / 86400)
+    let hours = Math.floor(time % 31536000 % 86400 / 3600)
+    let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
+    let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
+    days = days > 9 ? days : '0' + days
+    hours = hours > 9 ? hours : '0' + hours
+    minutes = minutes > 9 ? minutes : '0' + minutes
+    seconds = seconds > 9 ? seconds : '0' + seconds
+    return `${days > 0 ? `${days}:` : ''}${(hours || days) > 0 ? `${hours}:` : ''}${minutes}:${seconds}`
+}
+
+
+
+
+
+
+
+
+
 client.on('message', message => {
 	if(message.content.startsWith(prefix + 'quran')) {
 		message.delete();
@@ -264,9 +345,11 @@ client.on("message", message => {
      
  ❖  ${prefix}quran➾ لتشغيل القران
 
+ ❖  ${prefix}info➾ لمعلومات البوت
+
   ❖  ${prefix}support➾ لسيرفر سبورت البوت
 
- ❖  ${prefix}invite➾ لدعوه البوت للسيرفر
+ ❖  ${prefix}invite➾لدعوه البوت للسيرفر
    `)
    message.author.sendEmbed(embed)
    
